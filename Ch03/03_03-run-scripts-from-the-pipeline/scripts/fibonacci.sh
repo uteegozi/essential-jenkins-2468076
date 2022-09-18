1f6877aa0426313406616bce1989a7d47437a625
Ute Egozi <uegozi@redhat.com>
2022-09-18 12:04:49 +0300

    Update fibonacci.sh

--- Ch03/03_03-run-scripts-from-the-pipeline/scripts/fibonacci.sh
+++ Ch03/03_03-run-scripts-from-the-pipeline/scripts/fibonacci.sh
@@ -1,1 +1,19 @@
 
+#!/bin/bash
+
+echo "ute here"
+
+N=${1:-10}
+
+a=0
+b=1
+
+echo "The Fibonacci series is : "
+
+for (( i=0; i<N; i++ ))
+do
+    echo -e "$i\t$a"
+    fn=$((a + b))
+    a=$b
+    b=$fn
+done
